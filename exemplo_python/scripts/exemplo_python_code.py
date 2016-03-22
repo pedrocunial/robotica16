64cc53ca8a6610c3905c6b9f699d0c2b42864093
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-03-22 16:26:13 -0300

    Exemplo de movimentação proporcional -- An example of proportional movement

--- exemplo_python/scripts/exemplo_python_code.py
+++ exemplo_python/scripts/exemplo_python_code.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-# -*- coding:utf-8 -*- 
+# -*- coding:utf-8 -*-
 
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from math import fabs
+from sensor_msgs.msg import LaserScan
 
 
 velocidade_objetivo = Twist();
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=3)
 
 def notificacao(data):
+    global velocidade_objetivo
     """
         Codigo de notificacao executado sempre que chega uma leitura da odometria
 
@@ -18,11 +20,17 @@ def notificacao(data):
     """
     # Todo: a partir de uma leitura da odometria faça
     # um publish na velocidade até que o robô tenha andado 2 metros
-    
+    velocidade_x = data.ranges[0]
+    if velocidade_x <= 0.35:
+        velocidade_x = 0
+    # print(velocidade_x)
+    print("callback ")
     velocidade_objetivo = Twist()
-    velocidade_objetivo.linear.x = 0
+    velocidade_objetivo.linear.x = velocidade_x / 4
     velocidade_objetivo.linear.y = 0
     velocidade_objetivo.linear.z = 0
+    # print "A velocidade objetivo bla bla", velocidade_objetivo.linear.x
+
 
 
 
@@ -33,10 +41,15 @@ def controle():
         Função inicial do programa
     """
     rospy.init_node('Exemplo_Python')
-    rospy.Subscriber('/odom', Odometry, notificacao)
-    # Initial movement.    
+    rospy.Subscriber('/stable_scan', LaserScan, notificacao)
+    # Initial movement.
     pub.publish(velocidade_objetivo)
-    rospy.spin() # Faz um loop infinito para o ROS nao retornar
+    print("bla")
+    while not rospy.is_shutdown():  # Faz um loop infinito para o ROS nao retornar
+        rospy.sleep(0.2)
+        print velocidade_objetivo.linear.x
+        pub.publish(velocidade_objetivo)
+
 
 
 if __name__ == '__main__':
