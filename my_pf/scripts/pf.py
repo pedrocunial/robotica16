30f79794b72d2c7c2e404ebd574e8e83dc8b0eca
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-04-14 07:42:21 -0300

    Trabalhando no meu filtro de particulas

--- my_pf/scripts/pf.py
+++ my_pf/scripts/pf.py
@@ -11,14 +11,18 @@ import rospy
 
 from std_msgs.msg import Header, String
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped, PoseArray, Pose, Point, Quaternion
+from geometry_msgs.msg import (PoseStamped,
+                               PoseWithCovarianceStamped,
+                               PoseArray, Pose, Point,
+                               Quaternion)
 from nav_msgs.srv import GetMap
 from copy import deepcopy
 
 import tf
 from tf import TransformListener
 from tf import TransformBroadcaster
-from tf.transformations import euler_from_quaternion, rotation_matrix, quaternion_from_matrix
+from tf.transformations import (euler_from_quaternion,
+                                rotation_matrix, quaternion_from_matrix)
 from random import gauss, randint
 
 import math
@@ -34,29 +38,36 @@ from helper_functions import (convert_pose_inverse_transform,
                               convert_pose_to_xy_and_theta,
                               angle_diff)
 
+
 class Particle(object):
-    """ Represents a hypothesis (particle) of the robot's pose consisting of x,y and theta (yaw)
+
+    """
+        Represents a hypothesis (particle) of the robot's pose
+        consisting of x,y and theta (yaw)
         Attributes:
             x: the x-coordinate of the hypothesis relative to the map frame
             y: the y-coordinate of the hypothesis relative ot the map frame
             theta: the yaw of the hypothesis relative to the map frame
-            w: the particle weight (the class does not ensure that particle weights are normalized
+            w: the particle weight (the class does not ensure that particle
+                                                    weights are normalized
     """
 
-    def __init__(self,x=0.0,y=0.0,theta=0.0,w=1.0):
+    def __init__(self, x=0.0, y=0.0, theta=0.0, w=1.0):
         """ Construct a new Particle
             x: the x-coordinate of the hypothesis relative to the map frame
             y: the y-coordinate of the hypothesis relative ot the map frame
             theta: the yaw of the hypothesis relative to the map frame
-            w: the particle weight (the class does not ensure that particle weights are normalized """
+            w: the particle weight (the class does not ensure that particle
+                                                    weights are normalized """
         self.w = w
         self.theta = theta
         self.x = x
         self.y = y
 
     def as_pose(self):
-        """ A helper function to convert a particle to a geometry_msgs/Pose message """
-        orientation_tuple = tf.transformations.quaternion_from_euler(0,0,self.theta)
+        """ A helper function to convert a
+        particle to a geometry_msgs/Pose message """
+        orientation_tuple = tf.transformations.quaternion_from_euler(0, 0, self.theta)
         return Pose(position=Point(x=self.x,y=self.y,z=0), orientation=Quaternion(x=orientation_tuple[0], y=orientation_tuple[1], z=orientation_tuple[2], w=orientation_tuple[3]))
 
     # TODO: define additional helper functions if needed
@@ -108,6 +119,8 @@ class ParticleFilter:
         self.pose_listener = rospy.Subscriber("initialpose", PoseWithCovarianceStamped, self.update_initial_pose)
         # publish the current particle cloud.  This enables viewing particles in rviz.
         self.particle_pub = rospy.Publisher("particlecloud", PoseArray, queue_size=10)
+        # ?????
+        rospy.Subscriber('/simple_odom', OdometrySimple, self.process_odom)
 
         # laser_subscriber listens for data from the lidar
         self.laser_subscriber = rospy.Subscriber(self.scan_topic, LaserScan, self.scan_received)
@@ -118,6 +131,7 @@ class ParticleFilter:
 
         self.particle_cloud = []
         self.initial_particles = initial_list_builder()
+        self.particle_cloud.append([particle for particle in self.initial_particles])
 
         self.current_odom_xy_theta = []
 
@@ -128,25 +142,6 @@ class ParticleFilter:
         # for now we have commented out the occupancy field initialization until you can successfully fetch the map
         #self.occupancy_field = OccupancyField(map)
         self.initialized = True
-
-    def initial_list_builder(self):
-        '''
-        Creates the initial particles list,
-        using the super advanced methods
-        provided to us by the one and only
-        John
-        Cena
-        '''
-        initial_particles = []
-
-        for i in range(1000):
-            p = Particle()
-            p.x = randint(100) / 100
-            p.y = randint(100) / 100
-            p.theta = randint(359)
-            initial_particles.append(p)
-
-        return initial_particles
 
     def update_robot_pose(self):
         """ Update the estimate of the robot's pose given the updated particles.
@@ -169,18 +164,23 @@ class ParticleFilter:
 
             msg: this is not really needed to implement this, but is here just in case.
         """
-        new_odom_xy_theta = convert_pose_to_xy_and_theta(self.odom_pose.pose)
-        # compute the change in x,y,theta since our last update
-        if self.current_odom_xy_theta:
-            old_odom_xy_theta = self.current_odom_xy_theta
-            delta = (new_odom_xy_theta[0] - self.current_odom_xy_theta[0],
-                     new_odom_xy_theta[1] - self.current_odom_xy_theta[1],
-                     new_odom_xy_theta[2] - self.current_odom_xy_theta[2])
+        for particle in self.particle_cloud:
+            new_odom_xy_theta = convert_pose_to_xy_and_theta(self.odom_pose.pose)
+            # compute the change in x,y,theta since our last update
+            if self.current_odom_xy_theta:
+                old_odom_xy_theta = self.current_odom_xy_theta
+                delta = (new_odom_xy_theta[0] - self.current_odom_xy_theta[0],
+                         new_odom_xy_theta[1] - self.current_odom_xy_theta[1],
+                         new_odom_xy_theta[2] - self.current_odom_xy_theta[2])
 
-            self.current_odom_xy_theta = new_odom_xy_theta
-        else:
-            self.current_odom_xy_theta = new_odom_xy_theta
-            return
+                self.current_odom_xy_theta = new_odom_xy_theta
+            else:
+                self.current_odom_xy_theta = new_odom_xy_theta
+                return
+
+        # particle.x += delta[0]
+        # particle.y += delta[1]
+        # particle.theta += delta[2]
 
         # TODO: modify particles using delta
         # For added difficulty: Implement sample_motion_odometry (Prob Rob p 136)
@@ -307,10 +307,18 @@ class ParticleFilter:
               math.fabs(new_odom_xy_theta[1] - self.current_odom_xy_theta[1]) > self.d_thresh or
               math.fabs(new_odom_xy_theta[2] - self.current_odom_xy_theta[2]) > self.a_thresh):
             # we have moved far enough to do an update!
-            self.update_particles_with_odom(msg)    # update based on odometry
-            self.update_particles_with_laser(msg)   # update based on laser scan
+
+            '''
+
+                É AQUI!!!!
+
+            '''
+
+            self.update_particles_with_odom(msg)    # update based on odometry - FAZER
+            self.update_particles_with_laser(msg)   # update based on laser scan - FAZER
             self.update_robot_pose()                # update robot's pose
-            self.resample_particles()               # resample particles to focus on areas of high density
+            """ abaixo """
+            self.resample_particles()               # resample particles to focus on areas of high density - FAZER
             self.fix_map_to_odom_transform(msg)     # update map to odom transform now that we have new particles
         # publish particles (so things like rviz can see them)
         self.publish_particles(msg)
@@ -336,6 +344,26 @@ class ParticleFilter:
                                           rospy.get_rostime(),
                                           self.odom_frame,
                                           self.map_frame)
+
+def initial_list_builder():
+    '''
+    Creates the initial particles list,
+    using the super advanced methods
+    provided to us by the one and only
+    John
+    Cena
+    '''
+    initial_particles = []
+
+    for i in range(1000):
+        p = Particle()
+        p.x = randint(0, 100) / 100
+        p.y = randint(0, 100) / 100
+        p.theta = randint(0, 359)
+        initial_particles.append(p)
+
+    return initial_particles
+
 
 if __name__ == '__main__':
     n = ParticleFilter()
