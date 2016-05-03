f2b133702f03451103a7179a47c71f00ffa292e6
Pedro Cunial <pedrocc4@al.insper.edu.br>
2016-05-03 16:08:19 -0300

    atendimento

--- visao1/videoplay.py
+++ visao1/videoplay.py
@@ -24,20 +24,18 @@ while(True):
 
     red_image = output
 
-    # print(__doc__)
 
-
-    dst = cv2.Canny(frame, 100, 200) # aplica o detector de bordas de Canny à imagem src
+    dst = cv2.Canny(frame, 50, 300) # aplica o detector de bordas de Canny à imagem src
     cdst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR) # Converte a imagem para BGR para permitir desenho colorido
 
     if True: # HoughLinesP
         lines = cv2.HoughLinesP(dst, 1, math.pi/180.0, 40, np.array([]), 50, 10)
-        # print("Used Probabilistic Rough Transform")
-        # print("The probabilistic hough transform returns the end points of the detected lines")
-        a,b,c = lines.shape
-        for i in range(a):
-            # Faz uma linha ligando o ponto inicial ao ponto final, com a cor vermelha (BGR)
-            cv2.line(cdst, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.CV_AA)
+
+        if lines != None:
+            a,b,c = lines.shape
+            for i in range(a):
+                # Faz uma linha ligando o ponto inicial ao ponto final, com a cor vermelha (BGR)
+                cv2.line(cdst, (lines[i][0][0], lines[i][0][1]), (lines[i][0][2], lines[i][0][3]), (0, 0, 255), 3, cv2.CV_AA)
 
     else:    # HoughLines
         # Esperemos nao cair neste caso
@@ -59,7 +57,7 @@ while(True):
     final = cv2.bitwise_or(red_image, cdst)
     # Display the resulting frame
     cv2.imshow('a-ha - Take On Me (Official Video)',final)
-    # cv2.imshow('detected lines', cdst)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
